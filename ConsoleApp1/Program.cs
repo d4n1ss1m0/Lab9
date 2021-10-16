cd0c0a0d006f9d247b8303f927c0c6c6a38fa68c
Daniil <dan2002s@yandex.ru>
2021-10-16 14:12:25 +0300

    2.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            try
+            {
+                b.Quadr(0, 1, 1);
+                Console.WriteLine("D=" + b.getD());
+                Console.WriteLine("x1="+b.getX());
+                Console.WriteLine("x2=" + b.getX2());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
