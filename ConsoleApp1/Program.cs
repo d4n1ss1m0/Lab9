4bd6b64ad194834dc7bb3a0ac8576dad5f8a2de9
Daniil <dan2002s@yandex.ru>
2021-10-16 14:26:05 +0300

    3.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using core;
+
+namespace Siniak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SiniakLog log = SiniakLog.I(); 
+            Quadro b = new Quadro();
+            try
+            {                
+                b.Solve(0, 1, 1);
+                
+                log.log("x1=" + b.getX()[0].ToString());
+                if(b.getX().Length>1)
+                    log.log("x2=" + b.getX()[1].ToString());
+
+                log.write();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Siniak/Linear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniak
+{
+    class Linear
+    {
+        protected float[] x;
+        public float[] getX()
+        {
+            return x;
+        }
+
+        public float[] SolveLinear(float a, float b)
+        {
+            if (a == 0)
+            {
+                throw new Exception("x любое число");
+            }
+            this.x = new float[1];
+            return this.x = new float[1] { (-b) / a };
+        }
+    }
+}
